b1beeeec57795c08df5fd4e328c1c24721862085
JR <70702656+julienEcole@users.noreply.github.com>
2023-04-17 22:17:26 +0200

    feat : ajout connexion bdd, penssez a mettre vos liens de connexions, identifiant dans vos .env

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
 import {config} from "dotenv";
 config();
+import mongoose from "mongoose"
+import { Model } from "mongoose";
 import * as express from 'express'
 const app = express()
+
+async function startServer():Promise<void> {
+    const connexion = await mongoose.connect(process.env.MONGODB_URI as string,{
+        auth:{
+            username: process.env.MONGODB_USER as string,
+            password: process.env.MONGODB_PASSWORD as string
+        },
+        authSource: "root"
+    });
+    console.log(connexion);
+}
+
+//startServer()
 
 app.get('/', (req, res) => {
     res.status(200).send('OK on vas a la pèche')
@@ -10,3 +25,9 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT, () => {
     console.log('Server up on port ' + process.env.PORT);
 })
+
+startServer().catch(function(err):void{
+    console.log(err);
+});
+
+console.log("everything fine");
