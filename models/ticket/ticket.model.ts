f1623e5bd0938a8729ee4dba05de02898494d186
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-05-18 22:19:17 +0200

    Creation of the ticket model

---
+++ models/ticket/ticket.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema, Model } from "mongoose";
+import { Space } from "../space/space.model";
+
+const ticketShemma = new Schema<Ticket>({
+    type_ticket: {
+        type: Schema.Types.ObjectId,
+        required : true
+    },
+    accessible_spaces:[{
+        type: Schema.Types.ObjectId,
+        ref: "Space",
+        required: true
+    }],
+    is_in_use: {
+        type: Schema.Types.Boolean,
+        required: true
+    },
+    creation_date: {
+        type: Schema.Types.Date,
+        required : true
+    }
+}, {
+    versionKey: false,
+    collection: "Tickets"
+})
+
+export interface Ticket{
+    _id: string,
+    type_ticket: Object,
+    accessible_spaces: Space[],
+    is_in_use: boolean,
+    creation_date: Date
+}
+
+export const TicketModel: Model<Ticket> = mongoose.model("Ticket", ticketShemma)
