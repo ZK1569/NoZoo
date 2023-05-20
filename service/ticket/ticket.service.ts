993cb178954e87b6d4bfbc4271ee4721390a079f
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-05-20 13:11:21 +0200

    Check if the pass can go in the parc

--- service/ticket/ticket.service.ts
+++ service/ticket/ticket.service.ts
@@ -1,18 +1,50 @@
 import { Document, Model } from "mongoose"
-import { Ticket } from "../../models"
+import { Ticket, TypeTicketModel } from "../../models"
 
 export class TicketService {
     
     static canBeUse = async (ticket:Document<unknown, {}, Ticket> & Omit<Ticket & Required<{_id: string;}>, never>):Promise<boolean> => {
         
         const currentDate = new Date()
-        const typeTicket = ticket.type_ticket.name
+        const typeTicket = await TypeTicketModel.findById(ticket.type_ticket)
 
-        console.log(typeTicket);
+        switch(typeTicket?.name){
+            case "weekEnd":
+                // If the ticket is scanned during the weekend
+                if (!(6 <= currentDate.getDay() && currentDate.getDay() <= 7)){
+                    return false
+                }
+                
+                // No return true to check that the user scan his ticket in the day too
 
-        // TODO: Make a switch to do the verification on a case by case basis depending on the type of ticket
-        
-    
-        return false
+            case "escapeGame":
+            case "day": 
+
+                // If the ticket is scanned during the day
+                if(8 < currentDate.getHours() && currentDate.getHours() < 20){
+                    return true
+                }
+                
+                return false
+            
+            case "oneDayMonth":
+                console.log("C'est que un jour par mois, a ne pas supprimer");
+                return false
+                
+            case "night":
+                // If the ticket is not scanned during the day
+                if(!(8 < currentDate.getHours() && currentDate.getHours() < 20)){
+                    return true
+                }
+                
+                return false
+                
+            default:
+                console.log("ERROR: There was a problem with the verification of the tickets");
+                
+                return false
+                 
+        }
+
     }
 }
