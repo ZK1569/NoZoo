cccb72882a057a172e31be10f4aa7dfefebc8b77
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-05-28 11:53:12 +0200

    user created at the first start

--- index.ts
+++ index.ts
@@ -4,8 +4,9 @@ config();
 import * as express from 'express'
 import * as mongoose from 'mongoose'
 import { Response, Request} from "express"
+import { Document, Types } from "mongoose";
 import { UserController } from "./controller/user.controller";
-import { RoleModel, TicketModel, TypeTicketModel } from "./models";
+import { Role, RoleModel, TypeTicketModel, UserModel } from "./models";
 import { SpacesController } from "./controller/space/space.controller"
 import { AnimalController } from "./controller/space/animal.controller";
 import { AnimalGroupController } from "./controller/space/animalGroup.controller";
@@ -14,6 +15,7 @@ import { ZooController } from "./controller/administration/zoo.controller";
 import { ZooModel } from "./models/zoo.model";
 import { Employee_postModel } from "./models/administration/employee_post.model";
 import { TicketController } from "./controller/ticket/ticket.controller";
+import { SecurityUtils } from "./utils";
 
 const startServer = async (): Promise<void> => {
     const connection = await mongoose.connect(process.env.MONGODB_URI as string, {auth: {
@@ -26,6 +28,7 @@ const startServer = async (): Promise<void> => {
     await userRoles()
     await typeTickets()
     await zooCreation()
+    await createUsers()
     
     const app = express()
 
@@ -108,6 +111,32 @@ const zooCreation = async () => {
         visitorsLive: 0
     })
 
+}
+
+const createUsers = async ():Promise<void> => {
+    const countUsers = await UserModel.count().exec()
+    if(countUsers !== 0 ){
+        return 
+    }
+
+    const roles = await RoleModel.find().exec()
+    
+    const usersNames: string[] = ["admin@mail.com", "guest@mail.com"]
+
+    const usersRequest = usersNames.map((login) => {
+        let userRoles:(Document<unknown, {}, Role> & Omit<Role & {_id: Types.ObjectId;}, never>)[] = []
+        if (login.includes("admin")){
+            userRoles = roles
+        }else{
+            userRoles = roles.map((role) => {if(role.name === "guest"){return role} else {return null}}).filter((role) => {if(role){return role} else {return null}}) as (Document<unknown, {}, Role> & Omit<Role & {_id: Types.ObjectId;}, never>)[]
+        }
+        UserModel.create({
+            login,
+            password: SecurityUtils.toSHA512("password"),
+            roles: userRoles
+        })
+    })
+    await Promise.all(usersRequest)
 }
 
 startServer().catch((err) => {
